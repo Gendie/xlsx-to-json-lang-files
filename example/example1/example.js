817c7dd3b8a133ea828cd3272113394763ba443f
Gendy <mohamed.algendy@taj-it.com>
2019-04-21 13:36:01 +0200

    add example

---
+++ example/example1/example.js
@@ -0,0 +1,13 @@
+
+const xlsxToJson = require("../../lib/xlsx-to-json-lang-files");
+
+xlsxToJson(
+    './example-file.xlsx', 
+    './translation-output',
+    {
+        startColumn: 'A',   // Default is A
+        endColumn: 'F',     // Default is Z
+        startRow: 1,        // Default is 0
+        endRow: 'auto',        // Default is auto
+    }
+);
